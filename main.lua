a743415e082e124aa7701af6a0b8e6392947b504
Floris Kornelis van Dijken <badduckssb@gmail.com>
2023-01-07 02:34:12 +0100

    changes

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
+--the location of the game you want to continuously build
 local gamedir = "C:/Users/baddu/Documents/Love2D/test"
-local urfs = require("urfs")
 
-function love.draw()
-	love.graphics.print(
-	""..love.filesystem.getRealDirectory("").."\n"
-	..love.filesystem.getSource().."\n"
-	..love.filesystem.getAppdataDirectory().."\n"
-	..love.filesystem.getIdentity().."\n"
-	..love.filesystem.getSaveDirectory().."\n"
-	..love.filesystem.getSourceBaseDirectory().."\n"
-	..love.filesystem.getUserDirectory().."\n"
-	..love.filesystem.getWorkingDirectory().."\n"
-	, 100, 100)
+--mounting code
+local ffi = require "ffi"
+local C = ffi.os == "Windows" and ffi.load("love") or ffi.C
+ffi.cdef[[int PHYSFS_mount(const char *newDir, const char *mountPoint, int appendToPath);]]
+local mountedAs = {}
+if not C.PHYSFS_mount(gamedir, nil, 0) == 0 then
+    if not mountedAs[archive] then
+	    mountedAs[archive] = mountPoint or ""
+    end
 end
 
-urfs.mount(gamedir)
---package.loaded["main"] = nil
---require("main")
+--loading the game
+package.loaded["main"] = nil
+require("main")
+
+--TODO: code for reloading when reoping the app
