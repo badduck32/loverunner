e17c0ce4e49d2d205c39c92c1d364fcfd9fe0be7
Badduck <badduckssb@gmail.com>
2023-01-07 01:46:50 +0100

    test

--- main.lua
+++ main.lua
@@ -1,6 +1,19 @@
-local gamedir = "/storage/emulated/0/Git/love-chess/"
+local gamedir = "/storage/emulated/0/Git/chess-test"
 local urfs = require("urfs")
 
+function love.draw()
+	love.graphics.print(
+	""..love.filesystem.getRealDirectory("").."\n"
+	..love.filesystem.getSource().."\n"
+	..love.filesystem.getAppdataDirectory().."\n"
+	..love.filesystem.getIdentity().."\n"
+	..love.filesystem.getSaveDirectory().."\n"
+	..love.filesystem.getSourceBaseDirectory().."\n"
+	..love.filesystem.getUserDirectory().."\n"
+	..love.filesystem.getWorkingDirectory().."\n"
+	, 100, 100)
+end
+
 urfs.mount(gamedir)
-package.loaded["main"] = nil
-require("main")
+--package.loaded["main"] = nil
+--require("main")
